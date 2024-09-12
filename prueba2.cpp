0da6b09d94006a1de3f258d1b47950ccde068397
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2024-09-12 14:50:28 +0200

    pruebas

---
+++ prueba2.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+# include <cmath>
+
+int main()
+{
+	/*double posicion_inicial = 50.0;
+	double velocidad = -10.5;
+	double tiempo = 4.0;
+
+	double posicion_final = posicion_inicial + velocidad * tiempo;
+
+	std::cout << "La posicion final del objeto es: " << posicion_final << " metros." << std::endl;
+	*/
+
+	if (0.1  + 0.2 == 0.3)
+		std::cout << "perro" << std::endl;
+	return (0);
+}
